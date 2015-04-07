b88a7d735c3b6ff7563c61d8cf9e0a1ad7a2da5d
Ren Kan <rk6556@gmail.com>
2015-04-07 12:33:38 +0800

    init

---
+++ src/knowledgebase/ClientManagement.java
@@ -0,0 +1,115 @@
+package knowledgebase;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.franz.agraph.jena.AGGraph;
+import com.franz.agraph.jena.AGGraphMaker;
+import com.franz.agraph.jena.AGModel;
+import com.franz.agraph.jena.AGQuery;
+import com.franz.agraph.jena.AGQueryExecutionFactory;
+import com.franz.agraph.jena.AGQueryFactory;
+import com.franz.agraph.repository.AGCatalog;
+import com.franz.agraph.repository.AGRepository;
+import com.franz.agraph.repository.AGRepositoryConnection;
+import com.franz.agraph.repository.AGServer;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+
+public class ClientManagement {
+	public static final String SERVER_URL = "http://172.16.2.21:10035";
+	public static final String CATALOG_ID = "dbpedia2014";
+	public static final String REPOSITORY_ID = "dbpedia";
+	public static final String USERNAME = "super";
+	public static final String PASSWORD = "apex";
+	public static final String TEMPORARY_DIRECTORY = "";
+	
+	static final String RDFS_NS = "http://www.w3.org/2000/01/rdf-schema#";
+	static final String DBPEDIA_NS = "http://dbpedia.org/resource/";
+	static final String DBPEDIA_OWL_NS = "http://dbpedia.org/ontology/";
+	static final String DBPEDIA_PROP_NS = "http://dbpedia.org/property/";
+	
+	private static AGModel model = null;
+	
+	private static LinkedList<AGRepositoryConnection> toCloseConnection = new LinkedList<AGRepositoryConnection>();
+	
+	protected static void closeBeforeExit(AGRepositoryConnection conn) {
+		toCloseConnection.add(conn);
+	}
+	
+	static void close(AGRepositoryConnection conn) {
+		try {
+			conn.close();
+		} catch (Exception e) {
+			System.err.println("Error closing repository connection: "+e);
+			e.printStackTrace();
+		}
+	}
+	
+	protected static void closeAllConnections() {
+		for (AGRepositoryConnection conn : toCloseConnection) {
+			close(conn);
+		}
+	}
+	
+	public static AGModel getAgModel() throws Exception{
+		if(ClientManagement.model == null) {
+			AGServer server = new AGServer(SERVER_URL, USERNAME, PASSWORD);
+			System.out.println("Available catalogs: "+server.listCatalogs());
+			AGCatalog catalog = server.getCatalog(CATALOG_ID);
+			AGRepository repository = catalog.openRepository(REPOSITORY_ID);
+			AGRepositoryConnection connection = repository.getConnection();
+			AGGraphMaker maker = new AGGraphMaker(connection);
+			System.out.println("\nGot a graph maker for the connection.");
+			AGGraph graph = maker.getGraph();
+			AGModel model = new AGModel(graph);
+			ClientManagement.model = model;
+		}
+		return model;
+	}
+	
+	public AGRepositoryConnection getConnection() {
+		AGRepositoryConnection conn = null;
+		if(!toCloseConnection.isEmpty()) {
+			return toCloseConnection.getLast();
+		} else {
+			//TODO
+		}
+		return conn;
+	}
+	
+	public static void clearAll() {
+		closeAllConnections();
+		ClientManagement.model.close();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		AGModel model = ClientManagement.getAgModel();
+		
+		try {
+			String queryString = "SELECT ?s ?p  WHERE {"
+					+ "?s ?p <http://en.wikipedia.org/wiki/Netherlands>.} LIMIT 10";
+			AGQuery sparql = AGQueryFactory.create(queryString);
+			QueryExecution qe = AGQueryExecutionFactory.create(sparql, model);
+			try {
+				ResultSet results = qe.execSelect();
+				while (results.hasNext()) {
+					QuerySolution result = results.next();
+					RDFNode s = result.get("s");
+					RDFNode p = result.get("p");
+					// System.out.format("%s %s %s\n", s, p, o);
+					System.out.println(s + "\t" + p);
+				}
+			} finally {
+				qe.close();
+			}
+		} finally {
+			model.close();
+		}
+		
+	}
+
+}
---
+++ src/procedure/QuestionProcedure.java
@@ -0,0 +1,49 @@
+package procedure;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import org.omg.CORBA.PRIVATE_MEMBER;
+
+import basic.FileOps;
+
+/**
+ * Split question texts and remove entity mentions
+ * @author kren
+ *
+ */
+public class QuestionProcedure {
+
+	private String questionFile = "./data/questions.txt";
+	private String markedEntityFile = "./data/mark_entity.txt";
+	
+	private void questionSpliter() {
+		LinkedList<String> questions = FileOps.LoadFilebyLine(questionFile);
+		LinkedList<String> questionsSplitted = FileOps.LoadFilebyLine(questionFile);
+		LinkedList<String> questionsMarked = FileOps.LoadFilebyLine(markedEntityFile);
+		
+		int count = 0;
+//		Iterator<String> qIterator = questions.iterator();
+		Iterator<String> qmIterator = questionsMarked.iterator();
+		
+		while(qmIterator.hasNext()) {
+			String qmLine = qmIterator.next();
+			String[] qmItems = qmLine.split("\t");
+			int qId = Integer.parseInt(qmItems[0]);
+			int beginOffset = Integer.parseInt(qmItems[1]);
+			int endOffset = Integer.parseInt(qmItems[2]);
+			String question = questions.get(qId-1);
+			String questionSplitted = question.substring(0, beginOffset)+question.substring(endOffset+1, question.length());
+			questionsSplitted.remove(qId-1);
+			questionsSplitted.add(qId-1, questionSplitted);
+		}
+		FileOps.SaveList("./data/questions_splitted.txt", questionsSplitted);
+	}
+	
+	public static void main(String[] args) {
+		QuestionProcedure p = new QuestionProcedure();
+		p.questionSpliter();
+	}
+
+}
